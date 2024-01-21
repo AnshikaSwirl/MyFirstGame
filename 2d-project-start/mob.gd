3adbcf463bd7df2e84f611b251b84ad3b9a6e2b3
AnshikaSwirl <152862886+AnshikaSwirl@users.noreply.github.com>
2024-01-21 09:23:53 +0530

    Add files via upload

---
+++ 2d-project-start/mob.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+
+var player
+func _ready():
+	player=get_node("/root/Game/Player")
+
+
+func _physics_process(delta):
+	var direction = global_position.direction_to(player.global_position)
+	velocity= direction * 300.0
+	move_and_slide()
+
+
+
+
---
+++ 2d-project-start/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+
+func _physics_process(delta):
+	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
+	velocity =direction*600
+	move_and_slide()
+	
+	if velocity.length() > 0.0:
+		get_node("HappyBoo").play_walk_animation()
+	else:
+		get_node("HappyBoo").play_idle_animation()
+		
+
